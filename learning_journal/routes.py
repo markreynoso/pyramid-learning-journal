9deb3c4f7ef1dcf36302f9320992cb36181858ab
Mark Reynoso <mreynoso@spu.edu>
2017-10-31 18:22:08 -0700

    fixed edit.html page issue

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -5,6 +5,6 @@ def includeme(config):
     """Include the following routes for static files and uri paths."""
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('detail', '/journal/1')
+    config.add_route('detail', '/journal/{id:\d+}')
     config.add_route('create', '/journal/new-entry')
-    config.add_route('update', '/journal/1/edit-entry')
+    config.add_route('update', '/journal/{id:\d+}/edit-entry')
