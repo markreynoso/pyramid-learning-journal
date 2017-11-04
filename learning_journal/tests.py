dadd6f3091b4ecc3e600edb9a4581bf015cda3e5
Mark Reynoso <mreynoso@spu.edu>
2017-11-03 21:48:00 -0700

    wrote unit tests, all passing!

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -52,9 +52,18 @@ def test_list_view_returns_list_of_journals_in_dict(dummy_request):
     assert isinstance(response, dict)
 
 
-def test_journal_exists(dummy_request):
+def test_list_view_returns_empty_dict_with_no_entered_data(dummy_request):
+    """Test if list view returns dictionary with nothing in it if empty."""
+    from learning_journal.views.default import list_view
+    response = list_view(dummy_request)
+    assert len(response['blogs']) == 0
+
+
+def test_list_view_contains_new_data_added(dummy_request):
+    """Test if data sent through the request is added to the db."""
     from learning_journal.views.default import list_view
     new_entry = Blog(
+        id=100,
         title='Something Awesome',
         creation_date=11-12-2019,
         body='All the cool things I write.'
@@ -65,58 +74,83 @@ def test_journal_exists(dummy_request):
     assert new_entry.to_dict() in response['blogs']
 
 
+def test_detail_view_returns_dict(dummy_request):
+    """Test if detail view returns dictionary."""
+    from learning_journal.views.default import detail_view
+    new_detail = Blog(
+        id=101,
+        title='Something Awesomer',
+        creation_date=11-12-2019,
+        body='All the cool things I write.'
+    )
+    dummy_request.dbsession.add(new_detail)
+    dummy_request.dbsession.commit()
+    dummy_request.matchdict['id'] = 101
+    response = detail_view(dummy_request)
+    assert isinstance(response, dict)
 
 
-
-# def test_list_view_returns_dict():
-#     """Test if list view returns a dictionary."""
-#     from learning_journal.views.default import list_view
-#     req = DummyRequest()
-#     response = list_view(req)
-#     assert isinstance(response, dict)
-
-
-# def test_list_view_returns_list_of_journals_in_dict():
-#     """Test if list view returns list of blogs."""
-#     from learning_journal.views.default import list_view
-#     req = DummyRequest()
-#     response = list_view(req)
-#     assert 'title' in response['blogs'][0]
-
-
-# def test_deatil_view_returns_single_journal():
-#     """Test if list view returns list of blogs."""
-#     from learning_journal.views.default import detail_view
-#     req = DummyRequest()
-#     req.matchdict['id'] = 1
-#     response = detail_view(req)
-#     assert 'Day 1 Journal' in response['blog']['title']
+def test_detail_view_returns_sinlgle_item(dummy_request):
+    """Test if detail view returns dictionary with contents of 'title'."""
+    from learning_journal.views.default import detail_view
+    new_detail = Blog(
+        id=102,
+        title='Something Awesomers',
+        creation_date=11-12-2019,
+        body='All the cool things I write.'
+    )
+    dummy_request.dbsession.add(new_detail)
+    dummy_request.dbsession.commit()
+    dummy_request.matchdict['id'] = 102
+    response = detail_view(dummy_request)
+    assert response['blog']['title'] == 'Something Awesomers'
 
 
-# def test_create_view_returns_dict():
-#     """Test if create view returns a dictionary."""
-#     from learning_journal.views.default import create_view
-#     req = DummyRequest()
-#     response = create_view(req)
-#     assert isinstance(response, dict)
+def test_detail_view_raises_not_found_if_id_not_found(dummy_request):
+    """Test if detail raises HTTPNotFound if id not in dict."""
+    from learning_journal.views.default import detail_view
+    dummy_request.matchdict['id'] = 10000
+    with pytest.raises(HTTPNotFound):
+        detail_view(dummy_request)
 
 
-# def test_update_view_returns_dict():
-#     """Test if update view returns a dictionary."""
-#     from learning_journal.views.default import update_view
-#     req = DummyRequest()
-#     req.matchdict['id'] = 1
-#     response = update_view(req)
-#     assert isinstance(response, dict)
+def test_create_view_returns_dict(dummy_request):
+    """Test if create view returns a dictionary."""
+    from learning_journal.views.default import create_view
+    response = create_view(dummy_request)
+    assert isinstance(response, dict)
 
 
-# def test_update_view_raises_exception_id_not_found():
-#     """Test if update raises exception on non-existent id."""
-#     from learning_journal.views.default import update_view
-#     req = DummyRequest()
-#     req.matchdict['id'] = 20
-#     with pytest.raises(HTTPNotFound):
-#         update_view(req)
+def test_create_view_returns_empty_dict(dummy_request):
+    """Test if create view returns a dictionary."""
+    from learning_journal.views.default import create_view
+    response = create_view(dummy_request)
+    assert len(response) == 0
+
+
+def test_update_view_returns_dict(dummy_request):
+    """Test if update view returns a dictionary."""
+    from learning_journal.views.default import update_view
+    dummy_request.matchdict['id'] = 101
+    response = update_view(dummy_request)
+    assert isinstance(response, dict)
+
+
+def test_update_view_returns_title_of_single_entry(dummy_request):
+    """Test if update view title of single item chosen."""
+    from learning_journal.views.default import update_view
+    dummy_request.matchdict['id'] = 102
+    response = update_view(dummy_request)
+    assert response['blog']['title'] == 'Something Awesomers'
+
+
+def test_update_view_raises_exception_id_not_found(dummy_request):
+    """Test if update raises exception on non-existent id."""
+    from learning_journal.views.default import update_view
+    dummy_request.matchdict['id'] = 25
+    with pytest.raises(HTTPNotFound):
+        update_view(dummy_request)
+
 
 
 # @pytest.fixture
